bc9ff8784eecc0c93ecf9ad0383466e2d4072202
xuehaoweng <13721113750@163.com>
2023-05-23 14:10:45 +0800

    test-关闭任务清理函数

--- backend/IpamV1/celery.py
+++ backend/IpamV1/celery.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 import os
+from datetime import timedelta
+
 from django.apps import apps
 # set the default Django settings module for the 'celery' program.
 from django.utils import timezone
@@ -14,12 +16,21 @@ platforms.C_FORCE_ROOT = True
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IpamV1.settings')
 
 app = Celery('IpamV1')
+
 app.conf.ONCE = {
     'backend': 'celery_once.backends.Redis',
     'settings': {
         'url': settings.CELERY_ONCE_URL,
         'default_timeout': 60 * 60
     }
+}
+app.conf.beat_schedule = {
+    # Disable cleanup task by scheduling to run every ~1000 years
+    'backend_cleanup': {
+        'task': 'celery.backend_cleanup',
+        'schedule': timedelta(days=365*1000),
+        'relative': True,
+    },
 }
 app.now = timezone.now
 app.config_from_object('django.conf:settings', namespace='CELERY')
@@ -28,7 +39,7 @@ default_exchange = Exchange('default', type='direct')
 ipam_exchange = Exchange('ipam', type='direct')
 
 app.conf.task_time_limit = 86400
-app.conf.worker_prefetch_multiplier = 1
+app.conf.worker_prefetch_multiplier = 10
 app.conf.worker_max_tasks_per_child = 100
 app.conf.task_default_queue = 'default'
 app.conf.task_default_exchange = 'default'
