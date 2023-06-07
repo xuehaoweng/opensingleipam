f1f424b827ae927d556f6c2c0bda446aaec84354
xuehaoweng <13721113750@163.com>
2023-06-07 11:19:46 +0800

    'static'

--- backend/IpamV1/settings.py
+++ backend/IpamV1/settings.py
@@ -185,9 +185,9 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static")
+# ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 # STATIC_ROOT = '/static/'
 
